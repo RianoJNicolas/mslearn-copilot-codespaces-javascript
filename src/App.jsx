7164e70fc65736d32252aa23d162f2ed55d0953a
Johan Nicolas Riano Torres <rianojnicolas@gmail.com>
2023-11-27 18:28:53 +0000

    Animate social media icons with Copilot

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nicolas Riaño",
+  title: "Data Engineer & Electronics Engineer",
+  email: "rianojnicolas@gmail.com",
+  gitHub: "RianoJNicolas",
+  instagram: "rianojnicolas",
+  linkedIn: "Johan Nicolas Riaño Torres",
   medium: "",
-  twitter: "microsoft",
+  twitter: "riaanojnicolas",
   youTube: "Code",
 };
 
